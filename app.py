1844a3c17ecd2949a23db9b0a5eb776cc32f3664
hiir-21 <141605869+hiir-21@users.noreply.github.com>
2026-01-19 10:52:28 +0530

    Add app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Behavioural Robo-Advisory")
+
+st.title("Behavioural Bias Identification Dashboard")
+st.write("This dashboard provides behavioural diagnostics only.")
